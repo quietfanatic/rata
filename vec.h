bdacf071dadc9dab83eaa851b46ed8752ce9b215
quietfanatic <lewis@wall.org>
2011-11-28 12:24:56 -0800

    Add some geometric functions.

--- vec.h
+++ vec.h
@@ -20,33 +20,61 @@ struct Vec {
 const Vec Vec::undef = Vec(0/0.0, 0/0.0);
 
 
-const Vec operator - (Vec a) { return Vec(-a.x, -a.y); }
-const Vec operator + (Vec a, Vec b) { return Vec(a.x+b.x, a.y+b.y); }
-const Vec operator - (Vec a, Vec b) { return Vec(a.x-b.x, a.y-b.y); }
-const Vec operator * (Vec a, float b) { return Vec(a.x*b, a.y*b); }
-const Vec operator * (float a, Vec b) { return Vec(a*b.x, a*b.y); }
-const Vec operator / (Vec a,  float b) { return Vec(a.x/b, a.y/b); }
+Vec operator - (Vec a) { return Vec(-a.x, -a.y); }
+Vec operator + (Vec a, Vec b) { return Vec(a.x+b.x, a.y+b.y); }
+Vec operator - (Vec a, Vec b) { return Vec(a.x-b.x, a.y-b.y); }
+Vec operator * (Vec a, float b) { return Vec(a.x*b, a.y*b); }
+Vec operator * (float a, Vec b) { return Vec(a*b.x, a*b.y); }
+Vec operator / (Vec a,  float b) { return Vec(a.x/b, a.y/b); }
 Vec operator += (Vec& a, Vec b) { return a = a + b; }
 Vec operator -= (Vec& a, Vec b) { return a = a - b; }
 Vec operator *= (Vec& a, float b) { return a = a * b; }
 Vec operator /= (Vec& a, float b) { return a = a / b; }
 bool operator == (Vec a, Vec b) { return a.x==b.x && a.y==b.y; }
 bool operator != (Vec a, Vec b) { return a.x!=b.x || a.y!=b.y; }
-const float mag2 (Vec a) { return a.x*a.x+a.y*a.y; }
-const float mag (Vec a) { return sqrt(mag2(a)); }
-const float dot (Vec a, Vec b) { return a.x*b.x + a.y*b.y; }
-const float ang (Vec a) { return atan2(a.y, a.x); }
-const Vec norm (Vec a) { return a / mag(a); }
+float mag2 (Vec a) { return a.x*a.x+a.y*a.y; }
+float mag (Vec a) { return sqrt(mag2(a)); }
+float dot (Vec a, Vec b) { return a.x*b.x + a.y*b.y; }
+float ang (Vec a) { return atan2(a.y, a.x); }
+float slope (Vec a) { return a.y / a.x; }
+float liney (Vec a, Vec b, float x) {
+	return a.y + slope(b - a) * (x - a.x);
+}
+Vec norm (Vec a) { return a / mag(a); }
+Vec rotcw (Vec a) { return Vec(-a.y, a.x); }
 
-const bool defined (float a) { return a==a; }
-const bool defined (Vec a) { return defined(a.x) && defined(a.y); }
+bool defined (float a) { return a==a; }
+bool defined (Vec a) { return defined(a.x) && defined(a.y); }
 
 
-const bool in_rect (Vec a, Vec b, Vec c) {
-	return a.x >= b.x
-	    && a.x <= c.x
-	    && a.y >= b.y
-	    && a.y <= c.y;
+// Geometry
+
+bool in_rect (Vec p, Vec a, Vec b) {
+	return p.x >= a.x
+	    && p.x <= b.x
+	    && p.y >= a.y
+	    && p.y <= b.y;
+}
+
+bool across_line (Vec p, Vec a, Vec b) {
+	if (a.x == b.x)
+		return (a.y < b.y) ? p.x < a.x
+		                   : p.x > a.x;
+	float y = liney(a, b, p.x);
+	return (a.x < b.x) ? y < p.y
+	                   : y > p.y;
+}
+
+Vec uncross_line (Vec p, Vec a, Vec b) {
+	// To get the intersection of two lines, use
+	// ay + asx = by + bsx (y is y-intercept and s is slope)
+	// x = (by - ay) / (as - bs)
+	// y = liney(a, b, x)
+	if (a.x == b.x)
+		return Vec(a.x, p.y);
+	float x = (liney(a, b, 0) - liney(p, p + rotcw(b - a), 0))
+	        / (slope(b - a) - slope(rotcw(b - a)));
+	return Vec(x, liney(a, b, x));
 }
 
 
