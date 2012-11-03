ff232ffff2c8197bcd6c06ee0530ee79031da9dd
quietfanatic <lewis@wall.org>
2012-11-02 21:04:43 -0700

    Improve make system to allow recursion.

--- src/tap/make.pl
+++ src/tap/make.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 use lib 'tool';
-use make qw(rule phony make_argv);
+use make qw(workflow rule phony run);
 use autodie qw(:all);
 
 
@@ -11,40 +11,47 @@ my @compile = qw(g++-4.7 -std=c++11 -fmax-errors=5 -c);
 my @link = qw(g++-4.7);
 my @devel_flags = qw(-Wall -ggdb);
 my @libs = qw();
-my $run_testmain = './tap_testmain';
+sub execute { "./$_[0]" }
 sub output { '-o', $_[0] }
 
 my %modules = (
     'tap' => [qw(tap.h)],
     'tap_testmain' => [qw(tap.h)],
 );
+my %executables = (
+    'tap_testmain' => [qw(tap.o tap_testmain.o)],
+);
+
 my @objects = map "$_.o", keys %modules;
 
-sub compile { my @args = @_; system @compile, @args }
-sub _link { my @args = @_; system @link, @args }
+sub compile { my @args = @_; run @compile, @args }
+sub _link { my @args = @_; run @link, @args }
 
+workflow {
 
-phony 'test', 'tap_testmain', sub {
-    system "$run_testmain --test | prove -e '' -";
-};
-
-for my $m (keys %modules) {
-    rule "$m.o", ["$m.cpp", @{$modules{$m}}], sub {
-        compile("$m.cpp", @devel_flags, output("$m.o"))
+    phony 'test', 'tap_testmain', sub {
+        run execute('tap_testmain') . " --test | prove -e '' -";
     };
-}
 
-rule 'tap_testmain', \@objects, sub {
-    _link @objects, output('tap_testmain');
+    for my $module (keys %modules) {
+        rule "$module.o", ["$module.cpp", @{$modules{$module}}], sub {
+            compile "$module.cpp", @devel_flags, output("$module.o")
+        };
+    }
+    
+    for my $exe (keys %executables) {
+        rule $exe, $executables{$exe}, sub {
+            _link @{$executables{$exe}}, output($exe);
+        };
+    }
+
+    phony 'clean', [], sub {
+        no autodie;
+        unlink map "$_.o", keys %modules;
+        unlink keys %executables;
+    };
+
 };
 
-phony 'clean', [], sub {
-    no autodie;
-    unlink @objects;
-    unlink 'tap_testmain';
-};
-
-
-make_argv;
 
 
