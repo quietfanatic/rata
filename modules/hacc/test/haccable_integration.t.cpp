2a6cfa48fa51f085ee26603f32af35b5e60caa1b
quietfanatic <lewis@wall.org>
2012-12-12 00:30:44 -0800

    Fix tests with some awkward typecasted lamdbas, most of which in a slightly less awkward macro.

--- modules/hacc/test/haccable_integration.t.cpp
+++ modules/hacc/test/haccable_integration.t.cpp
@@ -6,11 +6,13 @@ int k = 0;
 int* kp = NULL;
 int* kp2 = NULL;
 
+#define SIRT(ret, params, ...) static_cast<ret(*)params>([]params{return (ret)__VA_ARGS__;})
+
 #include "../../tap/inc/tap.h"
 tap::Tester haccable_integration_tester ("haccable_integration", [](){
     using namespace hacc;
     using namespace tap;
-    plan(22);
+    plan(18);
 
     is(to_string<int>(12), String("12"), "to_string");
     is(from_string<int>("90"), (int)90, "from_string");
@@ -28,27 +30,27 @@ tap::Tester haccable_integration_tester ("haccable_integration", [](){
 
     remove("integration_test_file");
     doesnt_throw([](){hacc_to_file(Hacc(100), "integration_test_file");}, "hacc_to_file and...");
-    is_catch([](){return (int)hacc_from_file("integration_test_file").get_integer();}, (int)100, "... hacc_from_file");
+    is(SIRT(int, (), hacc_from_file("integration_test_file").get_integer()), (int)100, "... hacc_from_file");
     doesnt_throw([](){file_from_hacc("integration_test_file", Hacc(999));}, "file_from_hacc and...");
-    is_catch([](){return (int)file_to_hacc("integration_test_file").get_integer();}, 999, "... file_to_hacc");
+    is(SIRT(int, (), file_to_hacc("integration_test_file").get_integer()), 999, "... file_to_hacc");
 
     doesnt_throw([](){to_file<int>(13, "integration_test_file");}, "to_file and...");
-    is_catch([](){return (int)from_file<int>("integration_test_file");}, (int)13, "... from_file");
+    is(SIRT(int, (), from_file<int>("integration_test_file")), (int)13, "... from_file");
     doesnt_throw([](){file_from<int>("integration_test_file", -15);}, "file_from and...");
-    is_catch([](){return (int)file_to<int>("integration_test_file");}, -15, "... file_to");
-    is_catch([](){
+    is(SIRT(int, (), file_to<int>("integration_test_file")), -15, "... file_to");
+    is(static_cast<int(*)()>([](){
         update_from_file(&k, "integration_test_file");
         return k;
-    }, -15, "update_from_file");
-    is_catch([](){
+    }), -15, "update_from_file");
+    is(static_cast<int(*)()>([](){
         kp = new_from_file<int>("integration_test_file");
         return kp ? *kp : -99;
-    }, -15, "new_from_file");
+    }), -15, "new_from_file");
     free(kp);
-    is_catch([](){
+    is(static_cast<int(*)()>([](){
         kp2 = file_to_new<int>("integration_test_file");
         return kp2 ? *kp2 : -99;
-    }, -15, "file_to_new");
+    }), -15, "file_to_new");
     free(kp2);
     
 });
