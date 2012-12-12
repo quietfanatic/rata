952b089c793a14f77e5a191f69782886a33a8331
quietfanatic <lewis@wall.org>
2012-12-11 17:19:07 -0800

    Make haccable not depend on strings.

--- modules/hacc/inc/haccable.h
+++ modules/hacc/inc/haccable.h
@@ -3,7 +3,6 @@
 #include <string.h>
 
 #include "hacc.h"
-#include "strings.h"
 
 
 
@@ -313,28 +312,6 @@ template <class C> C* hacc_to_new (Hacc hacc) {
     return new_from_hacc<C>(hacc);
 }
 
- // Directly to and from strings.
-template <class C> String to_string (const C& v) {
-    return hacc_to_string(to_hacc<C>(v));
-}
-template <class C> C from_string (String s) {
-    return from_hacc<C>(hacc_from_string(s));
-}
-template <class C> void update_from_string (C* p, String s) {
-    update_from_hacc<C>(p, hacc_from_string(s));
-}
-template <class C> C string_to (String s) {
-    return from_string<C>(s);
-}
-template <class C> String string_from (const C& v) {
-    return to_string<C>(v);
-}
-template <class C> C* new_from_string (String s) {
-    return new_from_hacc<C>(hacc_from_string(s));
-}
-template <class C> C* string_to_new (String s) {
-    return new_from_string<C>(s);
-}
 
 }
 
--- modules/hacc/make.pl
+++ modules/hacc/make.pl
@@ -19,13 +19,13 @@ workflow {
 
     cppc_rule('tmp/hacc.o', [qw<src/hacc.cpp inc/hacc.h inc/options.h>]);
     cppc_rule('tmp/strings.o', [qw<src/strings.cpp inc/strings.h inc/hacc.h inc/options.h>]);
-    cppc_rule('tmp/haccable.o', [qw<src/haccable.cpp inc/haccable.h inc/strings.h inc/hacc.h inc/options.h>]);
-    cppc_rule('tmp/haccable_standard.o', [qw<src/haccable_standard.cpp inc/haccable_standard.h inc/haccable.h inc/strings.h inc/hacc.h inc/options.h>]);
+    cppc_rule('tmp/haccable.o', [qw<src/haccable.cpp inc/haccable.h inc/hacc.h inc/options.h>]);
+    cppc_rule('tmp/haccable_standard.o', [qw<src/haccable_standard.cpp inc/haccable_standard.h inc/haccable.h inc/hacc.h inc/options.h>]);
     cppc_rule('tmp/options.t.o', [qw<test/options.t.cpp inc/options.h>]);
     cppc_rule('tmp/hacc.t.o', [qw<test/hacc.t.cpp inc/hacc.h inc/options.h>]);
     cppc_rule('tmp/strings.t.o', [qw<test/strings.t.cpp inc/strings.h inc/hacc.h inc/options.h>]);
-    cppc_rule('tmp/haccable.t.o', [qw<test/haccable.t.cpp inc/haccable.h inc/strings.h inc/hacc.h inc/options.h>]);
-    cppc_rule('tmp/haccable_standard.t.o', [qw<test/haccable_standard.t.cpp inc/haccable_standard.h inc/haccable.h inc/strings.h inc/hacc.h>]);
+    cppc_rule('tmp/haccable.t.o', [qw<test/haccable.t.cpp inc/haccable.h inc/hacc.h inc/options.h>]);
+    cppc_rule('tmp/haccable_standard.t.o', [qw<test/haccable_standard.t.cpp inc/haccable_standard.h inc/haccable.h inc/hacc.h>]);
     ld_rule('tmp/t', [targetmatch(qr/^tmp\/[^\/]*\.o$/), '../tap/tmp/tap.o', '../tap/tmp/tap_make_test_main.o']);
 
     test_rule('tmp/t');
