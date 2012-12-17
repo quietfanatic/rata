6d1ae3a63cfcc941af687cfdb5504150a67b74b3
quietfanatic <lewis@wall.org>
2012-12-17 11:08:24 -0800

    Add hacc-files back in.

--- modules/hacc/make.pl
+++ modules/hacc/make.pl
@@ -18,7 +18,7 @@ workflow {
     include '../..';
 
     subdep('inc/strings.h', 'inc/hacc.h');
-    #subdep('inc/files.h', 'inc/hacc.h');
+    subdep('inc/files.h', 'inc/hacc.h');
     #subdep('inc/haccers.h', 'inc/hacc.h');
     #subdep('inc/haccable.h', 'inc/haccers.h');
     #subdep('inc/haccable_standard.h', 'inc/haccable.h');
@@ -26,12 +26,12 @@ workflow {
 
     cppc_rule('tmp/hacc.o', [qw<src/hacc.cpp inc/hacc.h>]);
     cppc_rule('tmp/strings.o', [qw<src/strings.cpp inc/strings.h>]);
-    #cppc_rule('tmp/files.o', [qw<src/files.cpp inc/files.h>]);
+    cppc_rule('tmp/files.o', [qw<src/files.cpp inc/files.h>]);
     #cppc_rule('tmp/haccable.o', [qw<src/haccable.cpp inc/haccable.h>]);
     #cppc_rule('tmp/haccable_standard.o', [qw<src/haccable_standard.cpp inc/haccable_standard.h>]);
     cppc_rule('tmp/hacc.t.o', [qw<test/hacc.t.cpp inc/hacc.h>]);
     cppc_rule('tmp/strings.t.o', [qw<test/strings.t.cpp inc/strings.h>]);
-    #cppc_rule('tmp/files.t.o', [qw<test/files.t.cpp inc/files.h>]);
+    cppc_rule('tmp/files.t.o', [qw<test/files.t.cpp inc/files.h>]);
     #cppc_rule('tmp/haccable.t.o', [qw<test/haccable.t.cpp inc/haccable.h>]);
     #cppc_rule('tmp/haccable_standard.t.o', [qw<test/haccable_standard.t.cpp inc/haccable_standard.h>]);
     #cppc_rule('tmp/haccable_integration.t.o', [qw<test/haccable_integration.t.cpp>]);
