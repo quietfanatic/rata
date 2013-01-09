51fdb0fc4cb628386a242bf0d856f917a72ba970
quietfanatic <lewis@wall.org>
2013-01-08 20:33:15 -0800

    Add default varrays and maps to haccable_standard

--- modules/hacc/inc/haccable_standard.h
+++ modules/hacc/inc/haccable_standard.h
@@ -9,4 +9,47 @@ template <class C> struct Haccable<C*> : hacc::Haccability<C*> {
     }
 };
 
+template <class C> struct Haccable<hacc::VArray<C>> : hacc::Haccability<hacc::VArray<C>> {
+    void describe (hacc::Haccer& h, hacc::VArray<C>& it) {
+        using namespace hacc;
+         // This is kind of silly honestly.
+        if (Haccer::Validator* v = h.validator()) {
+            it.assign(v->hacc.get_array().size(), C());
+        }
+        for (uint i = 0; h.writer() ? i < it.size() : h.elems_left(); i++) {
+            elem(it[i]);
+        }
+    }
+};
+
+template <class C> struct Haccable<hacc::Map<C>> : hacc::Haccability<hacc::Map<C>> {
+    void describe (hacc::Haccer& h, hacc::Map<C>& it) {
+        using namespace hacc;
+         // Okay this is really silly.
+        if (Haccer::Writer* w = h.writer()) {
+            for (auto iter = it.begin(); iter != it.end(); it++) {
+                attr(iter->first, iter->second);
+            }
+        }
+        else if (Haccer::Validator* r = h.validator()) {
+            auto& o = r->hacc.get_object();
+            for (auto iter = o.begin(); iter != o.end(); iter++) {
+                attr(iter->first, it[iter->first]);
+            }
+        }
+        else if (Haccer::Reader* r = h.reader()) {
+            auto& o = r->hacc.get_object();
+            for (auto iter = o.begin(); iter != o.end(); iter++) {
+                attr(iter->first, it[iter->first]);
+            }
+        }
+        else if (Haccer::Finisher* r = h.finisher()) {
+            auto& o = r->hacc.get_object();
+            for (auto iter = o.begin(); iter != o.end(); iter++) {
+                attr(iter->first, it[iter->first]);
+            }
+        }
+    }
+};
+
 #endif
