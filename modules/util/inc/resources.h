dacbcdf6c499c9135e99e7117e60bb9799beac11
quietfanatic <lewis@wall.org>
2013-01-13 02:20:21 -0800

    Fix non-un-reversal of status line in murphyme

--- modules/util/inc/resources.h
+++ modules/util/inc/resources.h
@@ -15,31 +15,5 @@ struct Resource {
     typedef Hash<Resource*> Table;
 };
 
- // Mixin for convenient haccability
-template <class C, class Base_Haccability>
-struct Resource_Haccability : Base_Haccability {
-    static C* find_by_haccid (Str id) {
-        C*& res = C::resource_table()[id];
-        if (res) {
-            return res;
-        }
-        else {
-            res = new C;
-            res->name = id;
-            if (res->load()) {
-                return res;
-            }
-            else {
-                delete res;
-                C::resource_table().erase(id);
-                return NULL;
-            }
-        }
-    }
-    static Str haccid (const C& res) {
-        return res.name;
-    }
-};
-
 
 #endif
