d30ea2d46fc273ad52f3d0858aac22aef33f8c5c
quietfanatic <lewis@wall.org>
2013-01-14 12:29:53 -0800

    [phys] Some more work on physics integration

--- modules/phys/inc/phys.h
+++ modules/phys/inc/phys.h
@@ -3,25 +3,24 @@
 
 namespace phys {
 
-     // This keeps track of all the state belonging to a physics simulation
-     //  ...which is...absolutely nothing.
-    struct Sim {
-    };
-
-    struct BodyInfo {
-    };
-
-     // This wraps a b2Body, and should be saved in the game state.
-    struct Body : b2Body {
-        const Vec& pos () { return reinterpret_cast<const Vec&>(GetPosition()); }
-        void set_pos (Vec p) { SetPosition(reinterpret_cast<const Vec&>(p)); }
-        const Vec& vel () { return reinterpret_cast<const Vec&>(GetLinearVelocity()); }
-        void set_vel (Vec v) { SetLinearVelocity(reinterpret_cast<const Vec&>(v)); }
-    };
+    extern b2World* sim;
 
     void init ();
 
     void step ();
+
+    struct FixtureDef {
+        b2FixtureDef b2;
+
+        b2Fixture* manifest (b2Body*);
+    };
+
+    struct BodyDef {
+        b2BodyDef b2;
+        std::vector<FixtureDef> fixtures;
+
+        b2Body* manifest (b2World* sim, Vec pos, Vec vel = Vec(0, 0));
+    };
 
 }
 
--- modules/phys/src/phys.cpp
+++ modules/phys/src/phys.cpp
@@ -10,13 +10,14 @@ HCB_END(b2Vec2)
 
 HCB_BEGIN(b2CircleShape)
     base<b2Shape>("circle");
-    attr("p", member(&b2CircleShape::m_p));
+    attr("c", member(&b2CircleShape::m_p));
     attr("r", member(&b2CircleShape::m_radius));
 HCB_END(b2CircleShape)
 
 HCB_BEGIN(b2PolygonShape)
     base<b2Shape>("polygon");
-    delegate(value_functions(
+    attr("radius", member(&b2PolygonShape::m_radius), b2_polygonRadius);
+    attr("verts", value_functions(
         [](const b2PolygonShape& ps){
             return std::vector<b2Vec2>(ps.m_vertices, ps.m_vertexCount);
         },
@@ -32,6 +33,61 @@ HCB_BEGIN(b2EdgeShape)
     elem(member(&b2EdgeShape::v2));
 HCB_END(b2EdgeShape)
 
+HCB_BEGIN(b2FixtureDef)
+    attr("shape", member(&b2FixtureDef::shape));
+    attr("friction", member(&b2FixtureDef::friction), 0.f);
+    attr("restitution", member(&b2FixtureDef::restitution), 0.f);
+    attr("density", member(&b2FixtureDef::density), 0.f);
+    attr("sensor", member(&b2FixtureDef::isSensor), false);
+//    attr("filter", member(&b2FixtureDef::filter))
+HCB_END(b2FixtureDef)
 
+HCB_BEGIN(phys::FixtureDef)
+    using namespace phys;
+    attr("b2", member(&FixtureDef::b2));
+HCB_END(phys::FixtureDef)
 
+HCB_BEGIN(b2BodyDef)
+    attr("type", member(&b2BodyDef::type), b2_dynamicBody);
+//    attr("pos", member(&b2BodyDef::position));  This shouldn't be set through this.
+//    attr("vel", member(&b2BodyDef::linearVelocity));  Nor this
+    attr("damping", member(&b2BodyDef::linearDamping), 0.f);
+    attr("gravity", member(&b2BodyDef::gravityScale), 0.f);
+    attr("unrotatable", member(&b2BodyDef::fixed_rotation), true);
+//    attr("ang", member(&b2BodyDef::angle), 0.f);
+//    attr("ang_vel", member(&b2BodyDef::angularVelocity), 0.f);
+HCB_END(b2BodyDef)
+
+HCB_BEGIN(phys::BodyDef)
+    using namespace phys;
+    attr("b2", member(&BodyDef::b2))
+    attr("fixtures", member(&BodyDef::fixtures))
+HCB_END(phys::BodyDef)
+
+namespace phys {
+    b2World* world;
+
+    void init () {
+        world = new b2World(
+            b2Vec2(-10, 0)
+        );
+    }
+
+    void step () {
+        world->step(1/60.0, 10, 10);
+    }
+
+    b2Fixture* FixtureDef::manifest (b2Body* b2b) {
+        b2Fixture* b2f = b2b->createFixture(b2);
+        b2f->setUserData(this);
+    }
+
+    b2Body* BodyDef::manifest (b2World* sim, Vec pos, Vec vel = Vec(0, 0)) {
+        b2Body* b2b = sim->createBody(&b2);
+        for (FixtureDef& fix : fixtures) {
+            fix->manifest(b2b);
+        }
+    }
+
+}
 
