4211d77baa3661ed366e2d886316d5de7dbd6f80
quietfanatic <lewis@wall.org>
2013-01-22 18:38:21 -0800

    [hacc] Indentation support in haccable_integration

--- modules/hacc/inc/haccable_integration.h
+++ modules/hacc/inc/haccable_integration.h
@@ -9,8 +9,8 @@
 namespace hacc {
 
  // Objects <-> Strings
-template <class C> String to_string (const C& v) {
-    return hacc_to_string(to_hacc<C>(v));
+template <class C> String to_string (const C& v, uint ind = 0) {
+    return hacc_to_string(to_hacc<C>(v), ind);
 }
 template <class C> C value_from_string (String s) {
     return std::move(value_from_hacc<C>(hacc_from_string(s)));
@@ -18,8 +18,8 @@ template <class C> C value_from_string (String s) {
 template <class C> C string_to_value (String s) {
     return value_from_string<C>(s);
 }
-template <class C> String string_from (const C& v) {
-    return to_string<C>(v);
+template <class C> String string_from (const C& v, uint ind = 0) {
+    return to_string<C>(v, ind);
 }
 template <class C> void update_from_string (C& p, String s) {
     update_from_hacc<C>(p, hacc_from_string(s));
@@ -32,22 +32,22 @@ template <class C> C* string_to_new (String s) {
 }
 
  // Haccs <-> Files
-static inline void hacc_to_file (Hacc* h, Either_String f) {
-    string_to_file(hacc_to_string(h), f);
+static inline void hacc_to_file (Hacc* h, Either_String f, uint ind = 0) {
+    string_to_file(hacc_to_string(h, ind), f);
 }
 static inline Hacc* hacc_from_file (Either_String f) {
     return hacc_from_string(string_from_file(f));
 }
-static inline void file_from_hacc (Either_String f, Hacc* h) {
-    return file_from_string(f, string_from_hacc(h));
+static inline void file_from_hacc (Either_String f, Hacc* h, uint ind = 0) {
+    return file_from_string(f, string_from_hacc(h, ind));
 }
 static inline Hacc* file_to_hacc (Either_String f) {
     return string_to_hacc(file_to_string(f));
 }
 
  // Objects <-> Files
-template <class C> void to_file (const C& v, Either_String f) {
-    hacc_to_file(to_hacc(v), f);
+template <class C> void to_file (const C& v, Either_String f, uint ind = 0) {
+    hacc_to_file(to_hacc(v), f, ind);
 }
 template <class C> C value_from_file (Either_String f) {
     return value_from_hacc<C>(hacc_from_file(f));
@@ -58,8 +58,8 @@ template <class C> void update_from_file (C& p, Either_String f) {
 template <class C> C file_to_value (Either_String f) {
     return value_from_file<C>(f);
 }
-template <class C> void file_from (Either_String f, const C& v) {
-    to_file<C>(v, f);
+template <class C> void file_from (Either_String f, const C& v, uint ind = 0) {
+    to_file<C>(v, f, ind);
 }
 template <class C> C* new_from_file (Either_String f) {
     return new_from_hacc<C>(hacc_from_file(f));
