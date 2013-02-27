c277e71dbd566010d44cc23de3873fe737639792
quietfanatic <lewis@wall.org>
2013-02-27 12:53:03 -0800

    [core] Add command_from_terminal back in temporarily

--- modules/core/src/input.cpp
+++ modules/core/src/input.cpp
@@ -25,6 +25,10 @@ namespace core {
                     quit_game();
                     break;
                 }
+                case '`': {
+                    command_from_terminal();
+                    break;
+                }
                 default: break;
             }
         }
