98c06df81b718bcd89409f0641f54d88e3d667b1
quietfanatic <lewis@wall.org>
2013-11-13 00:54:49 -0800

    [geo] Allow pushing camera in free_camera mode

--- modules/geo/src/camera.cpp
+++ modules/geo/src/camera.cpp
@@ -1,8 +1,10 @@
 
 #include "../inc/camera.h"
 #include "../inc/rooms.h"
+#include "../../core/inc/window.h"
 
 namespace geo {
+    using namespace core;
 
     bool camera_jump = false;
     bool free_camera = false;
@@ -16,7 +18,20 @@ namespace geo {
 
     Vec& update_camera () {
          // TODO: do stuff
-        if (free_camera) {
+        if (free_camera && !window->cursor_trapped) {
+            float move_speed = 1/20.0;
+            if (window->cursor_pos.x < 5) {
+                camera_pos.x += (window->cursor_pos.x - 5) * move_speed;
+            }
+            else if (window->cursor_pos.x > window->width*PX - 5) {
+                camera_pos.x += (window->cursor_pos.x - window->width*PX + 5) * move_speed;
+            }
+            if (window->cursor_pos.y < 5) {
+                camera_pos.y -= (window->cursor_pos.y - 5) * move_speed;
+            }
+            else if (window->cursor_pos.y > window->height*PX - 5) {
+                camera_pos.y -= (window->cursor_pos.y - window->height*PX + 5) * move_speed;
+            }
         }
         if (!camera_pos.is_defined())
             camera_pos = Vec(10, 7.5);
