7040e01a9293b2c08c0234a2e31e28bb2399f821
quietfanatic <lewis@wall.org>
2013-11-18 22:09:41 -0800

    [hacc] Don't fail rename if name is unchanged

--- modules/hacc/src/files.cpp
+++ modules/hacc/src/files.cpp
@@ -71,6 +71,7 @@ namespace hacc {
         return p->data.address();
     }
     void File::rename (std::string new_filename) {
+        if (new_filename == p->filename) return;
         if (File(new_filename).loaded()) throw X::File_Already_Loaded(new_filename);
         files_by_filename.erase(p->filename);
         p->filename = new_filename;
