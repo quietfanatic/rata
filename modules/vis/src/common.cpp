c4b4fb34707352cbe23dc94c5823a9c5eeb4a516
quietfanatic <lewis@wall.org>
2013-11-23 12:10:00 -0800

    [vis] Actually do the two-phase rendering.
    There's a camera bug.

--- modules/vis/src/common.cpp
+++ modules/vis/src/common.cpp
@@ -22,11 +22,12 @@ namespace vis {
     GLuint world_depth_rb = 0;
     Vec rtt_camera_size = Vec(NAN, NAN);
 
+    core::Program* world_program = NULL;
+
      // Set up the requirements for a render-to-texture step
     void setup_rtt () {
         if (rtt_camera_size != camera_size) {
             rtt_camera_size = camera_size;
-            diagnose_opengl("before setting up render-to-texture capability");
             if (world_fb) glDeleteFramebuffers(1, &world_fb);
             if (world_tex) glDeleteTextures(1, &world_tex);
             if (world_depth_rb) glDeleteRenderbuffers(1, &world_depth_rb);
@@ -38,16 +39,12 @@ namespace vis {
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
             glBindTexture(GL_TEXTURE_2D, 0);
-            diagnose_opengl("after setting up rtt tex");
             glGenRenderbuffers(1, &world_depth_rb);
             glBindRenderbuffer(GL_RENDERBUFFER, world_depth_rb);
             glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, 2*rtt_camera_size.x/PX, 2*rtt_camera_size.y/PX);
-            diagnose_opengl("after setting up rtt rb");
             glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, world_depth_rb);
-            diagnose_opengl("after glFramebufferRenderbuffer");
              // Do we have to unbind world_tex first?
             glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, world_tex, 0);
-            diagnose_opengl("after glFramebufferTexture");
              // Do we have to do this here or when rendering?
             auto ca = GL_COLOR_ATTACHMENT0;
             glDrawBuffers(1, (const GLenum*)&ca);
@@ -64,6 +61,11 @@ namespace vis {
             images_init();
             text_init();
             tiles_init();
+            world_program = hacc::File("vis/res/world.prog").data().attr("prog");
+            hacc::manage(&world_program);
+            world_program->use();
+            glUniform1i(world_program->require_uniform("tex"), 0);
+            diagnose_opengl("after loading world.prog");
             initted = true;
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         }
@@ -73,6 +75,7 @@ namespace vis {
         init();
         setup_rtt();
          // For now, clear to 50% grey
+        glBindFramebuffer(GL_FRAMEBUFFER, world_fb);
         glClearColor(0.5, 0.5, 0.5, 1);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
          // Map and sprite rendering uses depth and no blend
@@ -87,9 +90,21 @@ namespace vis {
             i.Drawn_draw(Map());
         for (auto& i : Sprites::items)
             i.Drawn_draw(Sprites());
-         // Overlay rendering uses blend and no depth
         glDisable(GL_DEPTH_TEST);
+         // Now convert from world fb to window fb
+        world_program->use();
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glBindTexture(GL_TEXTURE_2D, world_tex);
+        Vec pts [4];
+        pts[0] = Vec(-1, -1);
+        pts[1] = Vec(1, -1);
+        pts[2] = Vec(1, 1);
+        pts[3] = Vec(-1, 1);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vec), pts);
+        glDrawArrays(GL_QUADS, 0, 4);
+         // Overlay rendering uses blend and no depth
         glEnable(GL_BLEND);
+        Program::unuse();
         for (auto& i : Overlay::items)
             i.Drawn_draw(Overlay());
          // Turn off camera
