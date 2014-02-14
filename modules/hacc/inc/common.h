e9189b7cd29b2b01b6e691cd5df1f218a47e8205
quietfanatic <lewis@wall.org>
2014-02-13 22:06:43 -0800

    [hacc] typedef uint.

--- modules/hacc/inc/common.h
+++ modules/hacc/inc/common.h
@@ -17,6 +17,7 @@ typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
+typedef unsigned int uint;
 
 namespace hacc {
     typedef decltype(nullptr) Null;
