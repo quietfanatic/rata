5d52af6e5e383f9af46e4ad0eddfc2e3741b9533
quietfanatic <lewis@wall.org>
2014-02-24 00:42:44 -0800

    [core] Make create_file command take separate type and data args

--- modules/core/src/window.cpp
+++ modules/core/src/window.cpp
@@ -334,12 +334,16 @@ void _files () {
 }
 New_Command _files_cmd ("files", "List all loaded files", 0, _files);
 
-void _create_file (std::string filename, const hacc::Dynamic& data) {
-    hacc::File(filename, data);
+void _create_file (std::string filename, std::string type_name, hacc::Tree data) {
+    auto type = hacc::Type(type_name);
+    void* p = operator new(type.size());
+    type.construct(p);
+    hacc::Reference(type, p).from_tree(data);
+    hacc::File(filename, hacc::Dynamic(type, p));
 }
 New_Command _create_file_cmd (
     "create_file", "Create a new file object.  It will not yet be saved to disk.",
-    2, _create_file
+    3, _create_file
 );
 
 void _add (hacc::Document* doc, std::string type, hacc::Tree data) {
