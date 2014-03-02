84dbf7ba70e44322ced77c94429d4868724fd74a
quietfanatic <lewis@wall.org>
2014-03-01 18:40:29 -0800

    [core] Reload more stuff, working around reload bug temporarily

--- modules/core/src/window.cpp
+++ modules/core/src/window.cpp
@@ -293,7 +293,8 @@ void _reload (std::string filename) {
         window->before_next_frame([](){
             hacc::file_transaction([](){
                 for (auto f : hacc::loaded_files()) {
-                    if (f.filename().find("/res/") != std::string::npos) {
+                    if (f.filename().find("/res/") != std::string::npos
+                     || f.filename().find("world/") != std::string::npos) {
                         hacc::reload(f);
                     }
                 }
