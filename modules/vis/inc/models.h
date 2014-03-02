80a2fbc5e8f65bc4e5c1f1804cd5b9c581989a83
quietfanatic <lewis@wall.org>
2014-03-01 19:21:03 -0800

    [vis] Remove Skel::Seg::parent.  All it was doing was making finish() non-idempotent.

--- modules/vis/inc/models.h
+++ modules/vis/inc/models.h
@@ -23,8 +23,6 @@ namespace vis {
             std::vector<Skel::Seg*> branches;
             Layout* layout;
             float z_offset;
-
-            Skel::Seg* parent = NULL;  // Null if root, set after creation
         };
 
         hacc::named_vector<Skel::Seg> segs;
