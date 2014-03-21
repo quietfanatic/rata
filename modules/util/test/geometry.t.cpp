3dfd100d53866c47d9af364878a711c58396e64d
quietfanatic <lewis@wall.org>
2014-03-20 17:19:39 -0700

    [util] Add test for Line::snap

--- modules/util/test/geometry.t.cpp
+++ modules/util/test/geometry.t.cpp
@@ -5,7 +5,7 @@ using namespace util;
 using namespace tap;
 
 tap::Tester geometry_tester ("util/geometry", [](){
-    plan(8);
+    plan(10);
     ok(Circle(Vec(2, 2), 2).covers(Vec(1, 1)), "Circle::covers can report true");
     ok(!Circle(Vec(2, 2), 2).covers(Vec(0, 1)), "Circle::covers can report false");
     ok(Line(Vec(0, 1), Vec(3, 1)).covers(Vec(2, 0)), "Line::covers horizontal true");
@@ -14,4 +14,7 @@ tap::Tester geometry_tester ("util/geometry", [](){
     ok(!Line(Vec(1, 0), Vec(1, 3)).covers(Vec(2, 2)), "Line::covers vertical false");
     ok(Line(Vec(0, 0), Vec(5, 5)).covers(Vec(0, 1)), "Line::covers diagonal true");
     ok(!Line(Vec(0, 0), Vec(5, 5)).covers(Vec(1, 0)), "Line::covers diagonal false");
+    Vec snapped = Line(Vec(0, 0), Vec(5, 5)).snap(Vec(2, 2));
+    within(snapped.x, 1, 0.01, "Line::snap seems to work (x)");
+    within(snapped.y, 1, 0.01, "Line::snap seems to work (y)");
 });
