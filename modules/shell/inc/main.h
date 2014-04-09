1618583d0eaf056f59376b1d46aa460e81518c7f
quietfanatic <lewis@wall.org>
2014-04-09 03:17:53 -0700

    [shell] Add state_document convenience function

--- modules/shell/inc/main.h
+++ modules/shell/inc/main.h
@@ -2,8 +2,13 @@
 #define HAVE_SHELL_MAIN_H
 
 #include "hacc/inc/files.h"
+#include "hacc/inc/documents.h"
 
 extern hacc::File main_file;
 extern hacc::File current_state;
+
+static inline hacc::Document* state_document () {
+    return current_state.data();
+}
 
 #endif
