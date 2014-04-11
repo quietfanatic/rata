d181de9db1f71d9d243e8e947200f2c7e13b11d6
quietfanatic <lewis@wall.org>
2014-04-11 03:53:52 -0700

    [ent] Flip Flyer horizontally

--- modules/ent/src/robots.cpp
+++ modules/ent/src/robots.cpp
@@ -189,8 +189,7 @@ namespace ent {
             auto def = get_def();
             float down_angle = angle_diff(angle(focus), -M_PI / 2);
             auto frame_i = lround(down_angle / M_PI * 8);
-            Vec scale = focus.x > 0 ? Vec(1, 1) : Vec(-1, 1);
-            log("robot", "[%f %f] [%f %f]", get_pos().x, get_pos().y, scale.x, scale.y);
+            Vec scale = focus.x > 0 ? Vec(-1, 1) : Vec(1, 1);
             draw_frame(&def->layout->frames[frame_i], def->texture, get_pos(), scale);
         }
         int32 life = 96;
